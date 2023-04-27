a54b52bd39f143a60cb541ef30ff7e4dfa97b8fa
ewertoncodes <ewertonorg@gmail.com>
2023-04-26 23:34:10 -0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,48 +1,58 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Tabela from './Tabela';
+import Tabela from "./Tabela";
+import Formulario from "./Formulario";
 
 class App extends Component {
   state = {
     autores: [
       {
-        nome: 'Paulo',
-        livro: 'React',
-        preco: '1000'
+        nome: "Paulo",
+        livro: "React",
+        preco: "1000",
       },
       {
-        nome: 'Daniel',
-        livro: 'Java',
-        preco: '99'
+        nome: "Daniel",
+        livro: "Java",
+        preco: "99",
       },
       {
-        nome: 'Marcos',
-        livro: 'Design',
-        preco: '150'
+        nome: "Marcos",
+        livro: "Design",
+        preco: "150",
       },
       {
-        nome: 'Bruno',
-        livro: 'DevOps',
-        preco: '100'
-      }        
-    ]
-}
+        nome: "Bruno",
+        livro: "DevOps",
+        preco: "100",
+      },
+    ],
+  };
 
-  removeAutor = index => {
+  escutadorDeSubmit = autor => {
+    this.setState({ autores: [...this.state.autores, autor] });
+  }
+  
+  removeAutor = (index) => {
     const { autores } = this.state;
 
     this.setState({
-        autores : autores.filter((autor, posAtual) => {
-            return posAtual !== index;
-        }),
+      autores: autores.filter((autor, posAtual) => {
+        return posAtual !== index;
+      }),
     });
-  }
+  };
 
   render() {
     return (
-<Tabela autores= {this.state.autores} removeAutor= {this.removeAutor} />    );
+      <div>
+      <Tabela autores={this.state.autores} removeAutor = { this.removeAutor }/> 
+      <Formulario escutadorDeSubmit={this.escutadorDeSubmit}/>
+      </div> 
+    );
   }
+
 }
 
 export default App;
